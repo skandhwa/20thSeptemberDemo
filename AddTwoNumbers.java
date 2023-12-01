39b523df8f99bd49891a5930d1e663258750242e
skandhwa <113922787+skandhwa@users.noreply.github.com>
2023-12-01 18:33:20 +0530

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package September;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+
+            int a=20;
+            int b=40;
+            int c=a+b;
+            System.out.println("The Sum is  " +c);
+		
+
+	}
+
+}
